1b60837088f52ab3a399c78f6bc13ad03ddf8870
sim1hill <simone.c.hill@gmail.com>
2015-05-30 22:39:49 -0400

    got gif search terms from box input

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -5,8 +5,19 @@ export default Ember.Controller.extend({
   
   actions: {
     getGif: function(){
-      debugger;
-      $.getJSON("http://localhost:3000/gifs?q=cats&format=json", function(result){
+      var gifsearchwords = $('#blog-content').text();
+      var searchslug = "";
+      if (gifsearchwords.split(" ").length > 0){
+        // debugger;
+        searchslug = gifsearchwords.split(" ").pop();
+        if (searchslug === "," || searchslug === "." || searchslug === "!" || searchslug === "?"){
+          searchslug = gifsearchwords.split(" ").slice(-1,-1);
+        }
+      } else {
+        searchslug = gifsearchwords
+      }
+      $.getJSON("http://localhost:3000/gifs?q=" + searchslug + "&format=json", function(result){
+              // debugger;
        var embedGif = result['urls'][0]
         $('#new-gif').append('<img src=' + embedGif + '>');
       })
